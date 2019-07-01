ecf169d44bee3e6bca28f8ed685773fa7b59f7b3
Ishaan Jain <31792878+IronClad1607@users.noreply.github.com>
2019-07-01 20:29:56 +0530

    Menu added

--- Othello/app/src/main/java/com/example/othello/GameActivity.kt
+++ Othello/app/src/main/java/com/example/othello/GameActivity.kt
@@ -3,6 +3,10 @@ package com.example.othello
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.core.content.edit
 import kotlinx.android.synthetic.main.activity_game.*
 
@@ -11,6 +15,11 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        setSupportActionBar(tbGame)
+
+
+        supportActionBar?.title = "Othello!"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val pref = getPreferences(Context.MODE_PRIVATE)
 
@@ -19,9 +28,35 @@ class GameActivity : AppCompatActivity() {
         tvPl2.text = intent.getStringExtra("P2")
 
         pref.edit {
-            putString("P1",intent.getStringExtra("P1"))
-            putString("P2",intent.getStringExtra("P2"))
+            putString("P1", intent.getStringExtra("P1"))
+            putString("P2", intent.getStringExtra("P2"))
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.btnStop -> {
+            Toast.makeText(
+                this@GameActivity,
+                "Game Stopped",
+                Toast.LENGTH_LONG
+            ).show()
+            Handler().postDelayed({
+                finish()
+            }, 3000)
+            true
+        }
+        R.id.btnReset -> {
+            Toast.makeText(this, "Game Reset!", Toast.LENGTH_SHORT).show()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
